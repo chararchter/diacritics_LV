9e71885cb54dc40888604f7b9445956a830a3d59
chararchter <chararchter@pm.me>
2026-05-22 21:04:11 +0300

    Add docstrings and clean prints

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ import regex
 def read_csv(filepath: str) -> pd.DataFrame:
     return pd.read_csv(filepath, encoding="utf-8")
 
-def has_letters(value):
+def has_letters(value: str) -> bool:
+    """Return True if the value contains at least one Unicode letter."""
     return bool(regex.search(r"\p{L}", str(value)))
 
-def has_diacritics(value):
+def has_diacritics(value: str) -> bool:
+    """Return True if the value contains any Latvian diacritic character."""
     latvian_diacritics = "ĀāĒēĪīŪūČčĢģĶķĻļŅņŠšŽžŖŗ"
     return any(char in latvian_diacritics for char in str(value))
 
 def clean_names(df: pd.DataFrame) -> pd.DataFrame:
-    na_rows = df[df["Vardi"].isna()]
-    print("Rows with missing names:")
-    print(na_rows)
+    """
+    Remove rows with missing or letter-free names.
+    Returns the cleaned DataFrame with additional has_letters column (TODO: remove has_letters)
+    """
+
     # Drop NA
     df: DataFrame = df.dropna(subset=["Vardi"])
 
     # Drop rows that don't have letters, e.g. "-"
     df["has_letters"] = df["Vardi"].apply(has_letters)
-
-    print("Rows with no letters:")
-    print(df[~df["has_letters"]])
 
     return df[df["has_letters"]]
 
@@ -33,20 +34,16 @@ def main():
     df = read_csv("vardi-20260101.csv")
     df = clean_names(df)
     df["has_diacritics"] = df["Vardi"].apply(has_diacritics)
-    print(df[df["has_diacritics"]])
-    print(df[~df["has_diacritics"]])
 
     with_diacritics = df.loc[df["has_diacritics"], "Skaits"].sum()
     without_diacritics = df.loc[~df["has_diacritics"], "Skaits"].sum()
+    total = df["Skaits"].sum()
+
+    diacritics_percentage = with_diacritics/total*100
 
     print(f"With diacritics: {with_diacritics}")
     print(f"Without diacritics: {without_diacritics}")
-
-    total = df["Skaits"].sum()
-    print(total)
-    print(with_diacritics + without_diacritics)
-
-    diacritics_percentage = with_diacritics/total*100
+    print(f"Total: {total}")
     print(diacritics_percentage)
 
 
